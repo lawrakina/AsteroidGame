d40093b7200e9d1d20e70f0283ffd979e77de430
lawrik <lawr_fenix@mail.ru>
2020-02-21 13:42:27 +0300

    Добавили игроков

--- Tests/TestConsole/Program.cs
+++ Tests/TestConsole/Program.cs
@@ -10,6 +10,25 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
+            Gamer gamer = new Gamer();
+            gamer.Name = "Игрок 1";
+            gamer.DayOfBirth = new DateTime(1990,6,16,12,5,5);
+
+            Gamer[] gamers = new Gamer[100];
+            for (var i = 0; i < gamers.Length; i++) {
+                var g = new Gamer();
+                g.Name = $"Gamer {i + 1}";
+                g.DayOfBirth = DateTime.Now;
+                gamers[i] = g;
+            }
+
+            Console.ReadLine();
+            
         }
+    }
+
+    class Gamer {
+        public string Name;
+        public DateTime DayOfBirth;
     }
 }
