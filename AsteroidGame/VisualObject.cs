23a56f6fea78958e0d710673769204588428a946
lawrik <lawr_fenix@mail.ru>
2020-02-26 13:46:09 +0300

    Базовый графический объект

---
+++ AsteroidGame/VisualObject.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsteroidGame
+{
+    class VisualObject
+    {
+        protected Point _Position;
+        protected Point _Direction;
+        protected Size _Size;
+
+        public VisualObject(Point Position, Point Direction, Size Size)
+        {
+            _Position = Position;
+            _Direction = Direction;
+            _Size = Size;
+        }
+
+        public void Draw(Graphics g)
+        {
+            g.DrawEllipse(Pens.White, 
+                _Position.X, _Position.Y, 
+                _Size.Width, _Size.Height);
+        }
+
+        public void Update()
+        {
+            _Position = new Point(
+                _Position.X + _Direction.X, 
+                _Position.X +_Position.Y);
+
+            if (_Position.X < 0)
+            {
+                _Direction = new Point(-_Direction.X, _Direction.Y);
+            }
+            if (_Position.Y < 0)
+            {
+                _Direction = new Point(_Direction.X, -_Direction.Y);
+            }
+            if (_Position.X < Game.Width)
+            {
+                _Direction = new Point(-_Direction.X, _Direction.Y);
+            }
+            if (_Position.Y < Game.Heght)
+            {
+                _Direction = new Point(_Direction.X, -_Direction.Y);
+            }
+        }
+    }
+}
