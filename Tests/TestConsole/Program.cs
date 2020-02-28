f33c7a0819ffffd936c10a62945af28b13f241fc
lawrik <lawr_fenix@mail.ru>
2020-02-28 14:14:17 +0300

    Рефакторинг

--- Tests/TestConsole/Program.cs
+++ Tests/TestConsole/Program.cs
@@ -10,11 +10,19 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
-           
+            var positions = new List<Vector2D>();
+            for(var i = 0; i < 10; i++)
+            {
+                positions.Add(new Vector2D(10,10));
+            }
+
+            //positions[3].X = 7;
 
             Console.ReadLine();
 
         }
     }
+
+
 
 }
