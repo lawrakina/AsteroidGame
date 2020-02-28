192b9b44c36f9e86e21fde607162d7b69674268f
lawrik <lawr_fenix@mail.ru>
2020-02-28 14:44:41 +0300

    Логгеры

---
+++ Tests/TestConsole/Loggers/Logger.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole.Loggers
+{
+    public abstract class Logger
+    {
+        public abstract void Log(string Message);
+
+        public void LogInformation(string Message)
+        {
+            Log($"[Info]:{Message}");
+        }
+
+        public void LogWarning(string Message)
+        {
+            Log($"[Warning]:{Message}");
+
+        }
+
+        public void LogError(string Message)
+        {
+            Log($"[Error]:{Message}");
+
+        }
+    }
+
+    public class ListLogger : Logger
+    {
+        private readonly List<string> _Messages = new List<string>();
+        public string[] Messages => _Messages.ToArray();
+        public override void Log(string Message)
+        {
+            _Messages.Add($"({DateTime.Now}){Message}");
+        }
+
+    }
+
+    public class FileLogger : Logger
+    {
+        private int _Index;
+        public string FilePath { get; }
+        public FileLogger(string FilePath)
+        {
+            this.FilePath = FilePath;
+        }
+        public override void Log(string Message)
+        {
+            System.IO.File.AppendAllText(FilePath, $"{++_Index}:{Message}\r\n");
+        }
+    }
+
+    public abstract class DebugLogger : Logger
+    {
+        public abstract void Log(string Message, string Category);
+    }
+
+    public class VisualStudioOutputLogger : DebugLogger
+    {
+        public override void Log(string Message, string Category)
+        {
+            System.Diagnostics.Debug.WriteLine($">>>>>> {Message}", Category);
+        }
+
+        public override void Log(string Message)
+        {
+            System.Diagnostics.Debug.WriteLine($">>>>>> {Message}");
+        }
+    }
+
+    public class TraceLogger : DebugLogger
+    {
+        public override void Log(string Message, string Category)
+        {
+            System.Diagnostics.Trace.WriteLine($">>>>>> {Message}", Category);
+        }
+
+        public override void Log(string Message)
+        {
+            System.Diagnostics.Trace.WriteLine($">>>>>> {Message}");
+        }
+    }
+}
--- Tests/TestConsole/Program.cs
+++ Tests/TestConsole/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,20 +10,37 @@ namespace TestConsole
     class Program
     {
         static void Main(string[] args)
-        {
+        {/*
             var positions = new List<Vector2D>();
             for(var i = 0; i < 10; i++)
             {
                 positions.Add(new Vector2D(10,10));
             }
 
-            //positions[3].X = 7;
+            //positions[3].X = 7;*/
+
+            //Logger logger = new ListLogger();
+            //Logger logger = new FileLogger("program.log");
+
+            Logger logger = new TraceLogger();
+
+            Trace.Listeners.Add(new TextWriterTraceListener("trace.log"));
+
+            logger.LogInformation("Start program");
+
+            for (var i = 0; i < 10; i++)
+                logger.LogInformation($"Do some work {i + 1}");
+
+            logger.LogWarning("Завершение работы приложения");
+
+            //var log_message = ((ListLogger)logger).Messages;
+
+            Trace.Flush();//сохраняем все из памяти
 
             Console.ReadLine();
 
         }
     }
 
-
-
+    
 }
