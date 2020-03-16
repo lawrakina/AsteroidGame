5f9f92e91b2c92871ab352e8b267841363323691
lawrik <lawr_fenix@mail.ru>
2020-03-16 10:40:44 +0300

    Пример привязки в интерфейсе и в коде

---
+++ BindingTest/BindingTest/MainWindow2.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace BindingTest
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow2.xaml
+    /// </summary>
+    public partial class MainWindow2 : Window
+    {
+        public MainWindow2()
+        {
+            InitializeComponent();
+
+            var horizontal_binding = new Binding();
+            horizontal_binding.ElementName = "HorisontalSlider";
+            horizontal_binding.Path = new PropertyPath("Value");
+
+            HorisontalValue.SetBinding(TextBlock.TextProperty, horizontal_binding);
+
+            var vertical_binding = new Binding();
+            vertical_binding.ElementName = "VerticalSlider";
+            vertical_binding.Path = new PropertyPath("Value");
+
+            VerticalValue.SetBinding(TextBlock.TextProperty, vertical_binding);
+
+
+        }
+        //создано не показано на экране
+        protected override void OnInitialized(EventArgs e)
+        {
+            base.OnInitialized(e);
+        }
+    }
+}
