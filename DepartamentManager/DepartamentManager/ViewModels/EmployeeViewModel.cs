d18c272dbbd6904ae34cca1b491a39029e87198c
lawrik <lawr_fenix@mail.ru>
2020-03-19 16:54:44 +0300

    Делаем по новому с семинара. К командам вернусь позже.

---
+++ DepartamentManager/DepartamentManager/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,48 @@
+﻿using DepartamentManager.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DepartamentManager.ViewModels
+{
+    class EmployeeViewModel : ViewModel
+    {
+        public int Id { get; set; }
+
+        private string _Name;
+        public string Name
+        {
+            get => _Name;
+            set => Set(ref _Name, value);
+        }
+
+        private string _SurName;
+        public string SurName
+        {
+            get => _SurName;
+            set => Set(ref _SurName, value);
+        }
+
+        private string _Patronymic;
+        public string Patronymic
+        {
+            get => _Patronymic;
+            set => Set(ref _Patronymic, value);
+        }
+
+        private DateTime _DayOfBirth;
+        public DateTime DayOfBirth
+        {
+            get => _DayOfBirth;
+            set
+            {
+                if (Set(ref _DayOfBirth, value))
+                    OnPropertyChanged(nameof(Age));
+            }
+        }
+        public int Age => (int)Math.Floor((DateTime.Now - DayOfBirth).TotalDays / 365);
+
+    }
+}
--- DepartamentManager/DepartamentManager/ViewModels/MainWindowViewModel.cs
+++ DepartamentManager/DepartamentManager/ViewModels/MainWindowViewModel.cs
@@ -4,6 +4,7 @@ using DepartamentManager.Models;
 using DepartamentManager.ViewModels.Base;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
@@ -15,8 +16,52 @@ namespace DepartamentManager.ViewModels
     {
         public string Title { get; set; } = "Работа с департаментами и сотрудниками";
 
+        private DepartamentViewModel _SelectedDepartament;
+        public DepartamentViewModel SelectedDepartament
+        {
+            get => _SelectedDepartament;
+            set => Set(ref _SelectedDepartament, value);
+        }
+
+        private EmployeeViewModel _SelectedEmployee;
+        public EmployeeViewModel SelectedEmployee
+        {
+            get => _SelectedEmployee;
+            set => Set(ref _SelectedEmployee, value);
+        }
+
+        private ObservableCollection<Departamen> _Departamens = new ObservableCollection<Departamen>();
+        public ObservableCollection<Departamen> Departamens
+        {
+            get => _Departamens;
+            set => Set(ref _Departamens, value);
+        }
+
+        private ObservableCollection<EmployeeViewModel> _Employees = new ObservableCollection<EmployeeViewModel>();
+        public ObservableCollection<EmployeeViewModel> Employees
+        {
+            get => _Employees;
+            set => Set(ref _Employees, value);
+        }
+
+        public MainWindowViewModel()
+        {
+            foreach (var employee in Enumerable.Range(1, 100).Select(i => new EmployeeViewModel()
+            {
+                Id = i,
+                Name = $"Имя {i}",
+                SurName = $"Фамилия {i}",
+                Patronymic = $"Отчество {i}",
+                DayOfBirth = DateTime.Now.Subtract(TimeSpan.FromDays(365 * rnd.Next(18, 50)))
+            }))
+            {
+                _Employees.Add(employee);
+            }
+        }
 
         private RelayCommand testCommand;
+        private Random rnd = new Random();
+
         public RelayCommand TestCommand
         {
             get
