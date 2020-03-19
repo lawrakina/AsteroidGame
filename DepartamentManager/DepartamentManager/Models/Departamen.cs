deafd3608ba6c25fd4e70e022abd9f3ed9b498d8
lawrik <lawr_fenix@mail.ru>
2020-03-19 15:01:37 +0300

    Пробую добавить команды, не получается. Жду комментариев и помощи

--- DepartamentManager/DepartamentManager/Models/Departamen.cs
+++ DepartamentManager/DepartamentManager/Models/Departamen.cs
@@ -1,16 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DepartamentManager.Models
 {
-    class Departamen
+    class Departamen : INotifyPropertyChanged
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public List<Employee> Employees { get; set; }
+        private List<Employee> _Employees;
+        public List<Employee> Employees { get => _Employees; set {
+                _Employees = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Name)));
+            } }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public override string ToString()
         {
             return $"Id:{Id}, Name:{Name}, Employees Count:{Employees.Count}";
--- DepartamentManager/DepartamentManager/ViewModels/MainWindowViewModel.cs
+++ DepartamentManager/DepartamentManager/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,10 @@
-﻿using DepartamentManager.Models;
+﻿using DepartamentManager.Command;
+using DepartamentManager.Data;
+using DepartamentManager.Models;
 using DepartamentManager.ViewModels.Base;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,11 +15,19 @@ namespace DepartamentManager.ViewModels
     {
         public string Title { get; set; } = "Работа с департаментами и сотрудниками";
 
-        
 
-        void FillData()
+        private RelayCommand testCommand;
+        public RelayCommand TestCommand
         {
-
+            get
+            {
+                return testCommand ??
+                    (testCommand = new RelayCommand(obj =>
+                    {
+                        //команда не запускается???????????????????
+                        Debug.Fail("12312312312312312");
+                    }));
+            }
         }
     }
 }
