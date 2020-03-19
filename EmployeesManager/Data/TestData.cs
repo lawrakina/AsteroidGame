ca55bbd1a7b4cb7008fb11116702929edfc793eb
lawrik <lawr_fenix@mail.ru>
2020-03-19 10:50:49 +0300

    пример работы со списочным типом данных элементы MVVM

---
+++ EmployeesManager/Data/TestData.cs
@@ -0,0 +1,22 @@
+﻿using EmployeesManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeesManager.Data
+{
+    static class TestData
+    {
+        public static List<Employee> Employees { get; } = Enumerable.Range(1, 100)
+            .Select(i => new Employee { 
+                Id = i,
+                Name = $"Имя {i}", 
+                SurName = $"Фамилия {i}", 
+                Patronymic = $"Отчество {i}", 
+                DayOfBirth = DateTime.Now.Subtract(TimeSpan.FromDays(365/6 * (i+18)))
+            })
+            .ToList();
+    }
+}
