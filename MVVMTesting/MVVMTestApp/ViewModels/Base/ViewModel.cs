0c629b2f55168f57d47b3cd3244a9bee8c0c2bf0
lawrik <lawr_fenix@mail.ru>
2020-03-19 11:32:15 +0300

    Сахар

--- MVVMTesting/MVVMTestApp/ViewModels/Base/ViewModel.cs
+++ MVVMTesting/MVVMTestApp/ViewModels/Base/ViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,12 +11,12 @@ namespace MVVMTestApp.ViewModels.Base
     abstract class ViewModel :INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
-        protected virtual void OnPropertyChanged(string PropertyName)
+        protected virtual void OnPropertyChanged([CallerMemberName]string PropertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(PropertyName)));
         }
 
-        protected virtual bool Set<T>(ref T field, T value, string PropertyName)
+        protected virtual bool Set<T>(ref T field, T value, string PropertyName = null)
         {
             if (Equals(field, value)) return false;
             field = value;
---
+++ MVVMTesting/MVVMTestApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+
+namespace MVVMTestApp.ViewModels
+{
+    class MainWindowViewModel
+    {
+        private Timer _Timer;
+        public string Title { get; set; } = "Заголовок окна проекта MVVM";
+
+        public DateTime CurrentTime { get; set; }
+        public MainWindowViewModel()
+        {
+            _Timer = new Timer(100) { AutoReset = true };
+            _Timer.Elapsed += OnTimerTick;
+            _Timer.Start();
+        }
+
+        private void OnTimerTick(object sender, ElapsedEventArgs e)
+        {
+            CurrentTime = DateTime.Now;
+        }
+    }
+}
